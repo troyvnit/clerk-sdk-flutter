30ba770e39883e2fb3ad015b4685c250ea333d77
IKesh Pack <ikesh@devangels.london>
2024-08-29 08:51:47 -0400

    chore: refactoring for testing clarity

--- packages/clerk_auth/test/integration/clerk_api/sign_in_test.dart
+++ packages/clerk_auth/test/integration/clerk_api/sign_in_test.dart
@@ -1,20 +1,18 @@
 import 'package:clerk_auth/clerk_api/clerk_api.dart';
 import 'package:dart_dotenv/dart_dotenv.dart';
-import 'package:logger/logger.dart';
 import 'package:test/test.dart';
 
 import '../../test_helpers.dart';
 
 void main() {
   late final Api api;
-  late final Logger logger;
 
   final env = TestEnv();
 
   setUpAll(() {
-    logger = Logger();
-
-    env.addAll(DotEnv(filePath: '.env.test').getDotEnv());
+    final dotEnv = DotEnv(filePath: '.env.test');
+    final values = dotEnv.getDotEnv();
+    env.addAll(values);
 
     api = Api(publicKey: env.publicKey, publishableKey: env.publishableKey);
   });

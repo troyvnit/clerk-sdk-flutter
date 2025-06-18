8f5fb3ed6d66504559451ecd147101b49663dc73
Troy Lee <troyuit@gmail.com>
2025-06-18 20:17:19 +0700

    Upgrade url_launcher

--- packages/clerk_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/clerk_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import app_links
 import file_selector_macos
 import path_provider_foundation
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
